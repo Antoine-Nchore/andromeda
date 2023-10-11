6a9e6987a93ac9123749b84f1fd0ad980b6a2d4b
nchore <nchoreanthony@gmail.com>
2023-10-11 21:44:50 +0300

    updated dom

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+// Fetching data from a public API
 document.addEventListener('DOMContentLoaded', () =>{
     getWeather();
 })
-function getWeather(city = 'NAirobi'){
+function getWeather(city = 'Amsterdam'){
     const myHeaders = new Headers();
 myHeaders.append("X-Api-Key", "i8/oOmZWiKWws6kFjI8lsg==fRITSduo7vzUa9Es");
-myHeaders.append("", "");
-
 const requestOptions = {
   method: 'GET',
   headers: myHeaders,
   redirect: 'follow'
 };
 
-fetch(`https://api.api-ninjas.com/v1/weather?city=$(city)`, requestOptions)
+fetch("https://api.api-ninjas.com/v1/weather?city=London", requestOptions)
   .then(response => response.json())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
 }
+//Accessind elements with the DOM
+
+const ourCard = document.querySelector('.card');
+const searchBtn = document.querySelector('.search');
+const Onclick = document.getElementsByTagName('button');
+const weatherImg = document.querySelector("weather-icon");
+const temperature = document.querySelector("temp");
+const location = document.querySelector("city");
+const waterVapor = document.querySelector("humidity");
+const windSpeed = document.querySelector("wind")
+
