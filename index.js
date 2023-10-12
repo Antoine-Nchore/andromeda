bc4aea7a60d77d4aaeb071c7a344173a8e492cb9
nchore <nchoreanthony@gmail.com>
2023-10-12 22:01:25 +0300

    added event listners

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  getWeather("New York");
+  
 });
 
 function getWeather(city) {
   const apiKey = "i8/oOmZWiKWws6kFjI8lsg==fRITSduo7vzUa9Es";
-  const url = "https://api.api-ninjas.com/v1/weather?city=city";
+  const url = `https://api.api-ninjas.com/v1/weather?city=${city}`;
 
   const myHeaders = new Headers();
   myHeaders.append("X-Api-Key", "i8/oOmZWiKWws6kFjI8lsg==fRITSduo7vzUa9Es");
@@ -23,10 +23,20 @@ function getWeather(city) {
         document.querySelector(".temp").textContent = `Temperature: ${data.temp}°C`;
         document.querySelector(".Humidity").textContent = `Humidity: ${data.humidity}%`;
         document.querySelector(".Wind").textContent = `Wind speed: ${data.wind_speed} km/hr`;
-        //document.querySelector(".description").textContent = `Weather: ${data.weather[0].description}`;
+      
       })
       .catch(error => console.log('error', error));
 }
+//Event Listners to the file
+  const searchButton = document.querySelector(".SearchHere");
+  searchButton.addEventListener('click',() => {
+    const inputField = document.querySelector("#inputfield");
+  getWeather(inputField.value);
+    //console.log(data);
+  } )
+  
+
+
 
 
 
